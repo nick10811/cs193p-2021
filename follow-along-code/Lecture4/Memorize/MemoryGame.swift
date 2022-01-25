0fc95151a0e3fea032f03df9a2425d296bb7d4f6
Nick Yang <nick10811@gmail.com>
2022-01-25 21:21:22 +0800

    follow-along-code: lecture4 part2

--- follow-along-code/Lecture4/Memorize/MemoryGame.swift
+++ follow-along-code/Lecture4/Memorize/MemoryGame.swift
@@ -8,30 +8,53 @@
 import Foundation
 
 // Model: data + logic
-struct MemoryGame<CardContent> { // generic over CardContent
+struct MemoryGame<CardContent> where CardContent: Equatable { // generic over CardContent
     private(set) var cards: Array<Card>
+    
+    private var indexOfTheOneAndOnlyFaceUpCard: Int?
     
     // HINT: struct default is immutable
     mutating func choose(_ card: Card) {
-        let chosenIndex = index(of: card) // indexOf is readable
-//        var chosenCard = cards[chosenIndex]
-        //        card.isFaceUp = !card.isFaceUp
-//        chosenCard.isFaceUp.toggle()
-        cards[chosenIndex].isFaceUp.toggle()
-
-        // `print` is a good way for debugging
-        // automatically display console: Xcode -> Preferences -> Behaviors -> Generates Outputs
-//        print("chosenCard = \(chosenCard)")
+//        if let chosenIndex = index(of: card) { // indexOf is readable
+//        if let chosenIndex = cards.firstIndex(where: { aCardInTheCardsArray in aCardInTheCardsArray.id == card.id }) { // more Englishy
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched
+        {
+            //        var chosenCard = cards[chosenIndex]
+            //        card.isFaceUp = !card.isFaceUp
+            //        chosenCard.isFaceUp.toggle()
+            
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                indexOfTheOneAndOnlyFaceUpCard = nil
+            } else {
+//                for index in 0..<cards.count {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+            }
+            cards[chosenIndex].isFaceUp.toggle()
+            
+            // `print` is a good way for debugging
+            // automatically display console: Xcode -> Preferences -> Behaviors -> Generates Outputs
+            //        print("chosenCard = \(chosenCard)")
+        }
+        
         print("\(cards)")
     }
     
-    func index(of card: Card) -> Int {
+    func index(of card: Card) -> Int? {
         for index in 0..<cards.count {
             if cards[index].id == card.id {
                 return index
             }
         }
-        return 0 // bogus!
+        return nil
     }
     
     init (numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
@@ -45,7 +68,7 @@ struct MemoryGame<CardContent> { // generic over CardContent
     }
     
     struct Card: Identifiable { // make struct can be identifiable
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
         var id: Int
