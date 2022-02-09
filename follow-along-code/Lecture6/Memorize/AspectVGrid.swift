5d196ebefebca61a70068b940bb3da177af12251
Nick Yang <nick10811@gmail.com>
2022-02-09 22:12:16 +0800

    follow-along-code: lecture6 adaptive width

--- follow-along-code/Lecture6/Memorize/AspectVGrid.swift
+++ follow-along-code/Lecture6/Memorize/AspectVGrid.swift
@@ -14,10 +14,12 @@ struct AspectVGrid<Item, ItemView>: View where ItemView: View, Item: Identifiabl
     var content: (Item) -> ItemView // View & some View can't be used here
     
     var body: some View {
-        let width: CGFloat = 100
-        LazyVGrid(columns: [adaptiveGridItem(width: width)], spacing: 0) {
-            ForEach(items) { item in
-                content(item).aspectRatio(aspectRatio, contentMode: .fit)
+        GeometryReader { geometry in
+            let width: CGFloat = widthThatFits(itemCount: items.count, in: geometry.size, itemAspectRatio: aspectRatio)
+            LazyVGrid(columns: [adaptiveGridItem(width: width)], spacing: 0) {
+                ForEach(items) { item in
+                    content(item).aspectRatio(aspectRatio, contentMode: .fit)
+                }
             }
         }
     }
@@ -28,6 +30,23 @@ struct AspectVGrid<Item, ItemView>: View where ItemView: View, Item: Identifiabl
         return gridItem
     }
     
+    private func widthThatFits(itemCount: Int, in size: CGSize, itemAspectRatio: CGFloat) -> CGFloat {
+        var columnCount = 1
+        var rowCount = itemCount
+        repeat {
+            let itemWidth = size.width / CGFloat(columnCount)
+            let itemHeigh = itemWidth / itemAspectRatio
+            if CGFloat(rowCount) * itemHeigh < size.height {
+                break
+            }
+            columnCount += 1
+            rowCount = (itemCount + (columnCount - 1)) / columnCount
+        } while columnCount < itemCount
+        if columnCount > itemCount {
+            columnCount = itemCount
+        }
+        return floor(size.width / CGFloat(columnCount))
+    }
 }
 
 //struct AspectVGrid_Previews: PreviewProvider {
