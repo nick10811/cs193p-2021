6af69a832b57d6f8a8b585f5f1360b0d8752b0da
Nick Yang <nick10811@gmail.com>
2022-02-19 22:48:05 +0800

    follow-along-code: lecture9 rename ContentView

--- follow-along-code/Lecture9/EmojiArt/EmojiArtDocumentView.swift
+++ follow-along-code/Lecture9/EmojiArt/EmojiArtDocumentView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiArtDocumentView.swift
 //  EmojiArt
 //
 //  Created by Nick Yang on 2022/2/19.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct EmojiArtDocumentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        EmojiArtDocumentView()
     }
 }
