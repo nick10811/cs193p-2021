8fb703093fe813d0efa7760a9960c13eb88efcf7
Nick Yang <nick10811@gmail.com>
2022-02-19 19:43:26 +0800

    follow-along-code: lecture9

---
+++ follow-along-code/Lecture9/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Nick Yang on 2022/2/19.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
