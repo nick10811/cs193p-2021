d0e9d067a5bd9d6a078f0a6831961d99dcf7f06e
Nick Yang <nick10811@gmail.com>
2022-02-19 22:17:39 +0800

    follow-along-code: lecture9 EmojiArtModel

---
+++ follow-along-code/Lecture9/EmojiArt/EmojiArtModel.swift
@@ -0,0 +1,44 @@
+//
+//  EmojiArtModel.swift
+//  EmojiArt
+//
+//  Created by Nick Yang on 2022/2/19.
+//
+
+import Foundation
+
+struct EmojiArtModel {
+    var background = Background.blank
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable {
+        let text: String
+        var x: Int
+        var y: Int
+        var size: Int
+        let id: Int // conform Identifiable
+        
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    init() { }
+    
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmoji(_ text: String, at location: (x: Int, y: Int), size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: location.x, y: location.y, size: size, id: uniqueEmojiId))
+    }
+    
+    enum Background {
+        case blank
+        case url
+        case imageData
+    }
+}
