d950288f6ec24c35209c640b977ef024c7edd330
Nick Yang <nick10811@gmail.com>
2022-02-19 22:25:41 +0800

    follow-along-code: lecture9 associated data

--- follow-along-code/Lecture9/EmojiArt/EmojiArtModel.Background.swift
+++ follow-along-code/Lecture9/EmojiArt/EmojiArtModel.Background.swift
@@ -11,8 +11,22 @@ extension EmojiArtModel {
     
     enum Background {
         case blank
-        case url
-        case imageData
+        case url(URL)
+        case imageData(Data)
+        
+        var url: URL? {
+            switch self {
+            case .url(let url): return url
+            default: return nil
+            }
+        }
+        
+        var imageData: Data? {
+            switch self {
+            case .imageData(let data): return data
+            default: return nil
+            }
+        }
     }
     
 }
