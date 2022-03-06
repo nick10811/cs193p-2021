520a6bd46314ed3deb2ea55d1b84e1e0badf4068
Nick Yang <nick10811@gmail.com>
2022-03-06 15:45:30 +0800

    follow-along-code: lecture12 removeEmojiSection

--- follow-along-code/Lecture12/EmojiArt/PaletteEditor.swift
+++ follow-along-code/Lecture12/EmojiArt/PaletteEditor.swift
@@ -17,6 +17,7 @@ struct PaletteEditor: View {
             //            TextField("Name", text: $palette.name)
             nameSection
             addEmojisSection
+            removeEmojiSection
         }
         .frame(minWidth: 300, minHeight: 350)
         // $ is binding palette.name & @State palette
@@ -47,6 +48,23 @@ struct PaletteEditor: View {
                 .removingDuplicateCharacters
         }
     }
+    
+    var removeEmojiSection: some View {
+        Section(header: Text("Remove Emoji")) {
+            let emojis = palette.emojis.removingDuplicateCharacters.map { String($0) }
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 40))]) {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                        .onTapGesture {
+                            withAnimation {
+                                palette.emojis.removeAll(where: { String($0) == emoji })
+                            }
+                        }
+                }
+            }
+            .font(.system(size: 40))
+        }
+    }
 }
 
 struct PaletteEditor_Previews: PreviewProvider {
@@ -54,5 +72,7 @@ struct PaletteEditor_Previews: PreviewProvider {
 //        Text("Fixe Me!")
         PaletteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 4)))
             .previewLayout(.fixed(width: 300, height: 350))
+        PaletteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 2)))
+            .previewLayout(.fixed(width: 300, height: 600))
     }
 }
