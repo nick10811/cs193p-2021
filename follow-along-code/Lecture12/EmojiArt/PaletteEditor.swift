b9922329662cfe188dc7f7f2b946f3fa1eed489a
Nick Yang <nick10811@gmail.com>
2022-03-06 15:41:39 +0800

    follow-along-code: lecture12 onChange(of:)

--- follow-along-code/Lecture12/EmojiArt/PaletteEditor.swift
+++ follow-along-code/Lecture12/EmojiArt/PaletteEditor.swift
@@ -16,6 +16,7 @@ struct PaletteEditor: View {
         Form {
             //            TextField("Name", text: $palette.name)
             nameSection
+            addEmojisSection
         }
         .frame(minWidth: 300, minHeight: 350)
         // $ is binding palette.name & @State palette
@@ -25,6 +26,25 @@ struct PaletteEditor: View {
     var nameSection: some View {
         Section(header: Text("Name")) {
             TextField("Name", text: $palette.name)
+        }
+    }
+    
+    @State private var emojiToAdd = ""
+    
+    var addEmojisSection: some View {
+        Section(header: Text("Add Emojis")) {
+            TextField("", text: $emojiToAdd)
+                .onChange(of: emojiToAdd) { emojis in
+                    addEmojis(emojis)
+                }
+        }
+    }
+    
+    func addEmojis(_ emojis: String) {
+        withAnimation {
+            palette.emojis = (emojis + palette.emojis)
+                .filter { $0.isEmoji }
+                .removingDuplicateCharacters
         }
     }
 }
