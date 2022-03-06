7d762f8954f64d956cec81e74d2c6d0c90cc3f8d
Nick Yang <nick10811@gmail.com>
2022-03-06 15:48:06 +0800

    follow-along-code: lecture12 List

---
+++ follow-along-code/Lecture12/EmojiArt/PaletteManager.swift
@@ -0,0 +1,29 @@
+//
+//  PaletteManager.swift
+//  EmojiArt
+//
+//  Created by Nick Yang on 2022/3/6.
+//
+
+import SwiftUI
+
+struct PaletteManager: View {
+    @EnvironmentObject var store: PaletteStore
+    
+    var body: some View {
+        List {
+            ForEach(store.palettes) { palette in
+                VStack(alignment: .leading) {
+                    Text(palette.name)
+                    Text(palette.emojis)
+                }
+            }
+        }
+    }
+}
+
+struct PaletteManager_Previews: PreviewProvider {
+    static var previews: some View {
+        PaletteManager()
+    }
+}
