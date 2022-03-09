5211b961ca0f1a84331c412bc1979f42ecfc1329
Nick Yang <nick10811@gmail.com>
2022-03-09 22:33:07 +0800

    follow-along-code: lecture14 App&Scene&WindowGroup

--- follow-along-code/Lecture14/EmojiArt/EmojiArtApp.swift
+++ follow-along-code/Lecture14/EmojiArt/EmojiArtApp.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 @main
+// App protocol
 struct EmojiArtApp: App {
     @StateObject var document = EmojiArtDocument()
     @StateObject var paletteStore = PaletteStore(named: "Default")
     
+    // Scene: contains the top-level View of application
     var body: some Scene {
         WindowGroup {
+            // this closure is creating one of those Scenes
+            // and use same VM in those Scense
             EmojiArtDocumentView(document: document)
                 .environmentObject(paletteStore)
         }
