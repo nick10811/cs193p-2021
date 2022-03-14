da9fa5a1adb5bb75eaef7fcd01bf635e2fa1f540
Nick Yang <nick10811@gmail.com>
2022-03-14 22:15:44 +0800

    follow-along-code: lecture16 create a platform-specific file (macOS target only)

---
+++ follow-along-code/Lecture16/macOS/macOS.swift
@@ -0,0 +1,13 @@
+//
+//  macOS.swift
+//  EmojiArt
+//
+//  Created by Nick Yang on 2022/3/14.
+//
+
+import SwiftUI
+
+// platform-specific code
+
+// typealias UIImage to NSImage on macOS only
+typealias UIImage = NSImage
